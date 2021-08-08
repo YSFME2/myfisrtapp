acb36aabf81a5f98d399c0ffd7e3772622a0d8cf
Youssef Shaaban <Youssef Shaaban@DESKTOP-MVTT69L>
2021-08-09 00:18:31 +0200

    Image Testing

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -22,82 +22,52 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Stack(
+                    alignment: AlignmentDirectional.bottomCenter,
+                    children: [
+                      Image(
+                        height:150,
+                        width: double.infinity,
+                        image: NetworkImage("https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512_960_720.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 5
+                            ),
+                            color: Colors.black.withOpacity(0.5),
+                            child: Text(
+                              "Youssef",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 60),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Shaaban",style: TextStyle(fontSize: 30),),
-              color: Colors.blue,
-            ),
-            Container(
-              child: Text("El Sayed",style: TextStyle(fontSize: 30),),
-              color: Colors.purple,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 30),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Shaaban",style: TextStyle(fontSize: 30),),
-              color: Colors.blue,
-            ),
-            Container(
-              child: Text("El Sayed",style: TextStyle(fontSize: 30),),
-              color: Colors.purple,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 30),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Shaaban",style: TextStyle(fontSize: 30),),
-              color: Colors.blue,
-            ),
-            Container(
-              child: Text("El Sayed",style: TextStyle(fontSize: 30),),
-              color: Colors.purple,
-            ),
-            Container(
-              child: Text("Youssef",style: TextStyle(fontSize: 30),),
-              color: Colors.grey,
-            ),
-            Container(
-              child: Text("Shaaban",style: TextStyle(fontSize: 30),),
-              color: Colors.blue,
-            ),
-            Container(
-              child: Text("El Sayed",style: TextStyle(fontSize: 30),),
-              color: Colors.purple,
-            ),
-          ],
+          ),
         ),
       ),
     );
